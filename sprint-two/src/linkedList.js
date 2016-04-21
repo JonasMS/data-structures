3cae98fb35cc052b07aa113422a58e1dd3aaeace
JonasMS <jonas.sota@gmail.com>
2016-04-21 08:47:43 -0700

    Complete linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,40 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // list.tail = new Node(value);
+
+    var newNode = new Node(value);
+
+    if( list.tail ){
+      list.tail.next = newNode;
+    } else {
+      list.head = newNode;
+    }
+    list.tail = newNode;
+
   };
 
   list.removeHead = function() {
+    var headValue = list.head.value;
+    list.head = list.head.next;
+    return headValue;
+
   };
 
   list.contains = function(target) {
-  };
+    // var start = list.head;
 
+    var search = function(node, val) {
+      if ( node.value === val ){
+        return true;
+      } else if (node.next) {
+        return search(node.next, val)
+      } else {
+        return false;
+      }
+    };
+    return search(list.head, target);
+  };
   return list;
 };
 
