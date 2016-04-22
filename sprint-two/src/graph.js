7ed7956809bef0b7ab7bf9ea392f35314847a852
jonasms <jonas.sota@gmail.com>
2016-04-21 20:46:02 -0700

    Complete rest of tests for graph.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,12 +3,7 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
-  // var storage = {};
 
-  // // storage.value = null;
-  // // storage.nodes = [];
-
-  // return storage;
 };
 
 Graph.prototype.storage = [];
@@ -16,7 +11,10 @@ Graph.prototype.storage = [];
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(val) {
-  this.storage.push(new Node(val));
+  var newNode = new Node;
+  newNode.value = val;
+  this.storage.push(newNode);
+
 };
 
 // ------------------------
@@ -28,16 +26,6 @@ Graph.prototype.contains = function(target) {
 // ------------------------
 // Removes a target from the graph.
 Graph.prototype.removeNode = function(target) {
-  //search for node object w/ value that === target
-  // //delete object
-  // for (var i = 0; i < this.storage.length; i++) {
-  //   // if (this.storage[i] === undefined){ debugger;}
-  //   if (this.storage[i].value === target) {
-  //     this.storage = this.deleteNode(this.storage, i);
-  //     break;
-  //   }
-  // }
-
   var index = this.searchForNode(this.storage, target);
   if ( index > -1 ) {
     this.storage = this.deleteNode(this.storage, index);
@@ -64,39 +52,89 @@ Graph.prototype.searchForNode = function(arr, target) {
   return -1;
 };
 
-// [1, 3, 4, 5, 5]
-    
-
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
-  //check if fromNode.nodes (fromNode = Node.value) contains toNode
-  //check if toNode.nodes contains fromNode
+Graph.prototype.hasEdge = function(startVal, endVal) {
+  //nodeOne/Two is the object in graph's storage that has value startVal/endVal
+    //nodeOne/Two are the two nodes that we are looking at to check for relationship/edges
+  var node1 = this.storage[this.searchForNode(this.storage, startVal)];
+  var node2 = this.storage[this.searchForNode(this.storage, endVal)];
+
+  return ( node1.contains(endVal) || node2.contains(startVal) );
+
+
+  // return node2 in node1.nodes || node1 in node2.nodes;
+
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
-  //
+Graph.prototype.addEdge = function(startVal, endVal) {
+  //find the nodes assoc. with start/end val
+    //push node1.nodes.push(node2)
+
+  var node1 = this.storage[this.searchForNode(this.storage, startVal)];
+  var node2 = this.storage[this.searchForNode(this.storage, endVal)];
+
+  node1.nodes.push(node2);
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {
+Graph.prototype.removeEdge = function(startVal, endVal) {
+  //go into node1 array, 
+    //find node2 
+    //move everything over one
+    //pop
+  var index = this.searchForNode(this.storage, startVal);
+  var node1 = this.storage[index]; //first node obj
+  if ( node1.contains(endVal) ) {
+    //remove endVal from node1.nodes
+    node1.nodes = node1.deleteNode(node1.nodes, index);
+  }
+
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  _.each(this.storage, function(node) {
+    cb(node.value);
+  });
 };
 
-var Node = function(value) {
-  var node = {
-    value: value,
-    nodes: []
-  };
 
-  return node;
+
+var Node = function() {
+
+};
+
+Node.prototype.value = null;
+
+Node.prototype.nodes = [];
+
+Node.prototype.contains = function(target) {
+  return this.searchForNode(this.nodes, target) > -1;
+};
+
+Node.prototype.searchForNode = function(arr, target) {
+  for (var i = 0; i < arr.length; i++) {
+    if ( arr[i].value === target ) {
+      return i;
+    }
+  }
+
+  return -1;
+};
+
+Node.prototype.deleteNode = function(arr, idx) {
+  var copy = arr.slice();
+  for (var i = idx; i < copy.length - 1; i++) {
+    copy[i] = copy[i + 1];
+
+  }
+  copy.pop();
+  return copy;
 };
 
 /*
